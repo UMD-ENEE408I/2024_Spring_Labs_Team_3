1145dc4a1b7e24054dfecf7ea974481876dfac84
pkrepkiy <krepkiyp@yahoo.com>
2024-03-26 13:48:29 -0400

    First, second + third robot skeleton code

---
+++ Project/Motors/src/primary.cpp
@@ -0,0 +1,281 @@
+#include <Arduino.h>
+
+#include <Adafruit_MCP3008.h>
+
+Adafruit_MCP3008 adc1;
+Adafruit_MCP3008 adc2;
+
+const unsigned int ADC_1_CS = 2;
+const unsigned int ADC_2_CS = 17;
+
+const unsigned int M1_IN_1 = 13;
+const unsigned int M1_IN_2 = 12;
+const unsigned int M2_IN_1 = 25;
+const unsigned int M2_IN_2 = 14;
+
+const unsigned int M1_IN_1_CHANNEL = 8;
+const unsigned int M1_IN_2_CHANNEL = 9;
+const unsigned int M2_IN_1_CHANNEL = 10;
+const unsigned int M2_IN_2_CHANNEL = 11;
+
+const unsigned int M1_I_SENSE = 35;
+const unsigned int M2_I_SENSE = 34;
+
+// const float M_I_COUNTS_TO_A = (3.3 / 1024.0) / 0.120;
+
+const unsigned int PWM_VALUE = 512; // Max PWM given 8 bit resolution
+
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 10;
+
+int adc1_buf[8];
+int adc2_buf[8];
+
+float mid = 6;
+int base_pid = 450;
+
+uint8_t lineArray[13]; 
+float previousPosition = 6;
+
+
+float error;
+float last_error;
+float total_error;
+
+float Kp = 25;
+float Kd = 500;
+float Ki = 0.01;
+
+void M1_backward() {
+  ledcWrite(M1_IN_1_CHANNEL, PWM_VALUE);
+  ledcWrite(M1_IN_2_CHANNEL, 0);
+}
+
+void M1_forward(int pwm_value) {
+  ledcWrite(M1_IN_1_CHANNEL, 0);
+  ledcWrite(M1_IN_2_CHANNEL, pwm_value);
+}
+
+void M1_stop() {
+  ledcWrite(M1_IN_1_CHANNEL, PWM_VALUE);
+  ledcWrite(M1_IN_2_CHANNEL, PWM_VALUE);
+}
+
+void M2_backward() {
+  ledcWrite(M2_IN_1_CHANNEL, PWM_VALUE);
+  ledcWrite(M2_IN_2_CHANNEL, 0);
+}
+
+void M2_forward(int pwm_value) {
+  ledcWrite(M2_IN_1_CHANNEL, 0);
+  ledcWrite(M2_IN_2_CHANNEL, pwm_value);
+}
+
+void M2_stop() {
+  ledcWrite(M2_IN_1_CHANNEL, PWM_VALUE);
+  ledcWrite(M2_IN_2_CHANNEL, PWM_VALUE);
+}
+
+
+void readADC() {
+  for (int i = 0; i < 8; i++) {
+    adc1_buf[i] = adc1.readADC(i);
+    adc2_buf[i] = adc2.readADC(i);
+  }
+}
+
+void digitalConvert(){
+  for (int i = 0; i < 7; i++) {
+    if (adc1_buf[i]<700) {
+      lineArray[2*i] = 1; 
+    } else {
+      lineArray[2*i] = 0;
+    }
+    // Serial.print(lineArray[2*i]); Serial.print("\t");
+    // Serial.print(adc1_buf[i]); Serial.print("\t");
+
+    if (i<6) {
+      if (adc2_buf[i]<700){
+        lineArray[2*i+1] = 1;
+      } else {
+        lineArray[2*i+1] = 0;
+      }
+      // Serial.print(lineArray[2*i+1]); Serial.print("\t");
+      // Serial.print(adc2_buf[i]); Serial.print("\t");
+    }
+  }
+}
+
+float getPosition(float previousPosition) {
+  
+  float pos = 0;
+  uint8_t white_count = 0;
+  for (int i = 0; i < 13; i++) {
+    if (lineArray[i] == 1) {
+      pos += i;
+      white_count+=1;
+    } 
+  }
+
+  // Serial.print("white: "); Serial.print(white_count); Serial.print("\t");
+  // Serial.print("pos: "); Serial.print(pos); Serial.println("\t");
+  if (white_count == 0) {
+    return previousPosition;
+  }
+  return pos/white_count;
+}
+
+
+
+
+
+
+
+void setup() {
+  // Stop the right motor by setting pin 14 low
+  // this pin floats high or is pulled
+  // high during the bootloader phase for some reason
+
+
+
+  pinMode(14, OUTPUT);
+  digitalWrite(14, LOW);
+  delay(100);
+
+  Serial.begin(115200);
+
+  ledcSetup(M1_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M1_IN_2_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_2_CHANNEL, freq, resolution);
+
+  ledcAttachPin(M1_IN_1, M1_IN_1_CHANNEL);
+  ledcAttachPin(M1_IN_2, M1_IN_2_CHANNEL);
+  ledcAttachPin(M2_IN_1, M2_IN_1_CHANNEL);
+  ledcAttachPin(M2_IN_2, M2_IN_2_CHANNEL);
+
+  adc1.begin(ADC_1_CS);  
+  adc2.begin(ADC_2_CS);
+
+  pinMode(M1_I_SENSE, INPUT);
+  pinMode(M2_I_SENSE, INPUT);
+
+
+}
+
+void loop() {
+
+  int t_start = micros();
+  readADC();
+  int t_end = micros();
+
+  digitalConvert();
+
+  float pos = getPosition(previousPosition);
+  previousPosition = pos;
+
+   error = pos - mid;
+   total_error += error;
+
+   int pid_value = Kp*error + Kd*(error-last_error) + Ki*total_error;
+   int right_motor = base_pid + pid_value;
+   int left_motor = base_pid - pid_value;
+
+   M1_forward(left_motor);
+   M2_forward(right_motor);
+
+
+  int mode = 0;
+
+
+
+  // The mode is switched each time the robot hits a fat block -PK
+
+
+ 
+
+  //Put switch statements here for modes
+
+switch(mode) {
+
+  // START - GUN IT FORWARD
+  case 0:
+
+    // ALL MOTORS FORWARD
+    // PID CONTROLLER
+
+    break;
+
+  // MAZE OF MANDALORE - MAZE MODE (PRIMARY GUNS FORWARD)
+  case 1:
+
+    // THE PRIMARY ROBOT SKIPS ALL OF THIS 
+
+
+    break;
+
+
+  // KESSEL RUN - PID CONTROLLER BROKEN PATH
+  case 2:
+
+    // TUNE PID CONTROLLER HERE
+
+    break;
+
+  //HOTH ASTEROID FIELD - AVOIDANCE MODE
+  case 3:
+
+    break;
+
+
+  // DUAL FATES - SOUND LOCALIZATION (PYTHON HERE)
+  case 4:
+
+    //call python
+
+    break;
+  
+  // GUN IT
+  case 5:
+
+    // ALL MOTORS FORWARD
+    // PID CONTROLLER
+
+
+    break;
+      
+
+  
+  // ENDOR DASH - GUN IT FORWARD UNTIL HIT MODE CHANGE
+  case 6:
+
+    // ALL MOTORS FORWARD
+    // MAKE SURE TO LINE UP ROBOT ON THE LINE
+
+
+    break;
+
+  // END ARENA- STOP
+  case 7:
+
+    // STOP ALL MOTORS
+
+    break;
+}
+
+// Serial.print("left: \t"); Serial.print(left_motor); Serial.print("\t");
+
+// Serial.print("right: \t"); Serial.print(right_motor); Serial.print("\t");
+
+  // Serial.print("time: \t"); Serial.print(t_end - t_start); Serial.print("\t");
+  // Serial.print("pos: \t"); Serial.print(pos);
+  // Serial.println();
+
+  // M1_forward(512);
+  // M2_forward(512);
+  last_error = error;
+
+  // delay(100);
+
+}
